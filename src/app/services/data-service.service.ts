fe7d365149f0fb8a8e141aa2c7b312447f7cae10
Ng Task Team <ngtask.sll@gmail.com>
2019-02-16 23:16:13 +0200

    refactor: separate the mock data into service

---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,159 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import * as moment from 'moment-timezone';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  constructor() { }
+
+  private tasks: BehaviorSubject<{
+    date: string;
+    tasks: {
+      type: string;
+      isFinished: boolean;
+      time: string;
+      title: string;
+      content: string;
+    }[]
+  }[]> = new BehaviorSubject(
+    [
+      {
+        date: moment().format('YYYY-MM-DD'),
+        tasks: [{
+          type: 'W',
+          isFinished: false,
+          time: '11:00am',
+          title: 'Design Catchup',
+          content: 'Some large content goes here we should pipe it to char limit like 32 and then show it in details.'
+        },
+        {
+          type: 'W',
+          isFinished: false,
+          time: '5:00am',
+          title: 'Task 2',
+          content: 'Content 2'
+        },
+        {
+          type: 'B',
+          isFinished: false,
+          time: '1:45pm',
+          title: 'Task 6',
+          content: 'Content 6'
+        },
+        {
+          type: 'P',
+          isFinished: false,
+          time: '8:00pm',
+          title: 'Task 7',
+          content: 'Content 7'
+        }]
+      },
+      {
+        date: moment().add(1, 'd').format('YYYY-MM-DD'),
+        tasks: [{
+          type: 'W',
+          isFinished: false,
+          time: '1:45pm',
+          title: 'Task 3',
+          content: 'Content 3'
+        },
+        {
+          type: 'P',
+          isFinished: false,
+          time: '2:45pm',
+          title: 'Task 4',
+          content: 'Content 4'
+        },
+        {
+          type: 'P',
+          isFinished: false,
+          time: '3:45pm',
+          title: 'Task 5',
+          content: 'Content 5'
+        }]
+      },
+      {
+        date: moment().add(2, 'd').format('YYYY-MM-DD'),
+        tasks: [{
+          type: 'B',
+          isFinished: false,
+          time: '4:45pm',
+          title: 'Task 8',
+          content: 'Content 8'
+        }]
+      },
+    ]
+  );
+
+  private meetings: BehaviorSubject<{
+    scheduled: any,
+    rescheduled: any,
+    cancelled: any,
+  }> = new BehaviorSubject({
+    scheduled: [
+      {
+        date: moment().format('YYYY-MM-DD'),
+        name: 'The meting',
+        time: '9:00am',
+        desc: 'Some random meeting',
+        people: ['some@random.mail'],
+      },
+      {
+        date: moment().format('YYYY-MM-DD'),
+        name: 'The second',
+        time: '10:00am',
+        desc: 'Some random meeting',
+        people: ['some@random.mail'],
+      },
+      {
+        date: moment().format('YYYY-MM-DD'),
+        name: 'The third',
+        time: '11:00am',
+        desc: 'Some random meeting',
+        people: ['some@random.mail'],
+      }
+    ],
+    rescheduled: [
+      {
+        date: moment().format('YYYY-MM-DD'),
+        name: 'New meting',
+        time: '11:00am',
+        desc: 'Second random meeting',
+        people: ['second@random.mail'],
+      }
+    ],
+    cancelled: [
+      {
+        date: moment().format('YYYY-MM-DD'),
+        name: 'New meting',
+        time: '9:00am',
+        desc: 'Some random meeting',
+        people: ['some@random.mail'],
+      }
+    ]
+  });
+
+  getTasks() {
+    return this.tasks.asObservable();
+  }
+
+  addTasks(task: any) {
+    const allTasks = this.tasks.value;
+    // TODO: mutate all tasks and add the new task
+    this.tasks.next(allTasks);
+  }
+
+  getMeetings() {
+    return this.meetings.asObservable();
+  }
+
+  addMeetings(meeting: any) {
+    const allMeetings = this.meetings.value;
+    // TODO: mutate all meetings and add the new meeting
+    this.meetings.next(allMeetings);
+  }
+}
--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TaskboardComponent } from '../taskboard/taskboard.component';
 import * as moment from 'moment-timezone';
+import { Subscription } from 'rxjs';
+import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
   selector: 'app-workspace',
@@ -9,130 +11,31 @@ import * as moment from 'moment-timezone';
 })
 
 export class WorkspaceComponent implements OnInit {
+  private taskSubscription: Subscription = new Subscription();
+  private meetingsSubscription: Subscription = new Subscription();
 
-  days = [
-    {
-      date: moment().format('YYYY-MM-DD'),
-      tasks: [{
-        type: 'W',
-        isFinished: false,
-        time: '11:00am',
-        title: 'Design Catchup',
-        subtitle: 'Subtitle desc',
-        content: 'Some large content goes here we should pipe it to char limit like 32 and then show it in details.'
-      },
-      {
-        type: 'W',
-        isFinished: false,
-        time: '5:00am',
-        title: 'Task 2',
-        subtitle: 'some',
-        content: 'Content 2'
-      },
-      {
-        type: 'B',
-        isFinished: false,
-        time: '1:45pm',
-        title: 'Task 6',
-        subtitle: 'some',
-        content: 'Content 6'
-      },
-      {
-        type: 'P',
-        isFinished: false,
-        time: '8:00pm',
-        title: 'Task 7',
-        subtitle: 'some',
-        content: 'Content 7'
-      }]
-    },
-    {
-      date: moment().add(1, 'd').format('YYYY-MM-DD'),
-      tasks: [{
-        type: 'W',
-        isFinished: false,
-        time: '1:45pm',
-        title: 'Task 3',
-        subtitle: 'some',
-        content: 'Content 3'
-      },
-      {
-        type: 'P',
-        isFinished: false,
-        time: '2:45pm',
-        title: 'Task 4',
-        subtitle: 'some',
-        content: 'Content 4'
-      },
-      {
-        type: 'P',
-        isFinished: false,
-        time: '3:45pm',
-        title: 'Task 5',
-        subtitle: 'some',
-        content: 'Content 5'
-      }]
-    },
-    {
-      date: moment().add(2, 'd').format('YYYY-MM-DD'),
-      tasks: [{
-        type: 'B',
-        isFinished: false,
-        time: '4:45pm',
-        title: 'Task 8',
-        subtitle: 'some',
-        content: 'Content 8'
-      }]
-    },
-  ].filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD'));
+  days = [];
 
-  meetings = {
-    scheduled: [
-      {
-        date: moment().format('YYYY-MM-DD'),
-        name: 'The meting',
-        time: '9:00am',
-        desc: 'Some random meeting',
-        people: ['some@random.mail'],
-      },
-      {
-        date: moment().format('YYYY-MM-DD'),
-        name: 'The second',
-        time: '10:00am',
-        desc: 'Some random meeting',
-        people: ['some@random.mail'],
-      },
-      {
-        date: moment().format('YYYY-MM-DD'),
-        name: 'The third',
-        time: '11:00am',
-        desc: 'Some random meeting',
-        people: ['some@random.mail'],
-      }
-    ].filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')),
-    rescheduled: [
-      {
-        date: moment().format('YYYY-MM-DD'),
-        name: 'New meting',
-        time: '11:00am',
-        desc: 'Second random meeting',
-        people: ['second@random.mail'],
-      }
-    ].filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')),
-    cancelled: [
-      {
-        date: moment().format('YYYY-MM-DD'),
-        name: 'New meting',
-        time: '9:00am',
-        desc: 'Some random meeting',
-        people: ['some@random.mail'],
-      }
-    ].filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')),
-  };
+  meetings = {};
 
-  constructor() { }
+  constructor(private dataServiceService: DataServiceService) { }
 
   ngOnInit() {
+    this.taskSubscription = this.dataServiceService.getTasks().subscribe( tasks => {
+      this.days = tasks.filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD'));
+    });
+
+    this.meetingsSubscription = this.dataServiceService.getMeetings().subscribe( meetings => {
+      this.meetings = {
+        scheduled: meetings.scheduled.filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')),
+        rescheduled: meetings.rescheduled.filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')),
+        cancelled: meetings.cancelled.filter(t => moment(t.date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')),
+      };
+    });
   }
 
+  ngOnDestroy() {
+    this.taskSubscription.unsubscribe();
+    this.meetingsSubscription.unsubscribe();
+  }
 }
